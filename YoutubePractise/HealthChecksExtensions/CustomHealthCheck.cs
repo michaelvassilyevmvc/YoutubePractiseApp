cfa2485cbd90057c8a31909519068da69b25877d
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2024-12-21 21:01:09 +0500

    Реализованы фичи по работе с HealthCheck

---
+++ YoutubePractise/HealthChecksExtensions/CustomHealthCheck.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace YoutubePractise.HealthChecksExtensions
+{
+    public class CustomHealthCheck: IHealthCheck
+    {
+        private Random _random = new Random();
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var responseTime = _random.Next(1, 300);
+            return Task.FromResult(responseTime switch
+            {
+                < 100 => HealthCheckResult.Healthy("Healthy result from CustomHealthChecks"),
+                (> 100) and (< 200) => HealthCheckResult.Degraded("Degrated result from CustomHealthCheck"),
+                _ => HealthCheckResult.Unhealthy("Unhealthy result from CustomHealthCheck")
+            });
+        }
+    }
+}
--- YoutubePractise/Program.cs
+++ YoutubePractise/Program.cs
@@ -1,6 +1,28 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using YoutubePractise.HealthChecksExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks()
+                .AddSqlServer(builder.Configuration.GetConnectionString("sqlConnection")!, name: "SqlHealth", tags: new List<string>() {"database"})
+                .AddCheck<CustomHealthCheck>("CustomHealthCheck", tags: new List<string>() { "custom" });
+builder.Services.AddHealthChecksUI()
+    .AddInMemoryStorage();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapHealthChecks("/health/custom", new HealthCheckOptions
+{
+    Predicate = reg => reg.Tags.Contains("custom"),
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapHealthChecksUI();
 
 app.Run();
