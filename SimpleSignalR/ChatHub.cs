2c3bd541980b084dfe9f3bb4942809ae14da6adb
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2024-12-28 22:51:30 +0500

    Simple Web Razor chat implementation

---
+++ SimpleSignalR/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SimpleSignalR
+{
+    public class ChatHub: Hub
+    {
+        public async Task JoinToChat(string chatName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatName);
+        }
+
+        public async Task SendMessage(string chatName,
+                                      string user,
+                                      string message)
+        {
+            await Clients.Group(chatName).SendAsync("ReceiveMessage"
+                , $"User {user} says - {message} from chat {chatName}");
+        }
+    }
+}
---
+++ SimpleSignalR/wwwroot/js/chat.js
@@ -0,0 +1,34 @@
+﻿//const { signalR } = require("./signalr/dist/browser/signalr");
+
+document.getElementById("messages").style.display = "none";
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chat")
+    .build();
+
+connection.start().catch(e => console.error(e.toString()));
+
+document.getElementById("joinButton")
+    .addEventListener("click", e => {
+        const chatName = document.getElementById("inputChat").value;
+
+        connection.invoke("JoinToChat", chatName).catch(e => console.errro(e.toString()));
+        document.getElementById("join").style.display = "none";
+        document.getElementById("messages").style.display = "block";
+        e.preventDefault();
+    });
+
+document.getElementById("sendButton")
+    .addEventListener("click", e => {
+        const chatName = document.getElementById("inputChat").value;
+        const user = document.getElementById("inputUser").value;
+        const message = document.getElementById("inputMessage").value;
+
+        connection.invoke("SendMessage", chatName, user, message).catch(e => console.errro(e.toString()));
+        e.preventDefault();
+    });
+
+connection.on("ReceiveMessage", message => {
+    var el = document.createElement("li");
+    document.getElementById("messageList").appendChild(el);
+    el.textContent = message;
+})
